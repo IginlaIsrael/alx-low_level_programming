cfa6d88e15e6434cf16c1179f8340fe63a691fae
IginlaIsrael <iginlaisraelo@gmail.com>
2023-03-20 14:50:57 -0700

    confused a bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-	printf("Size of a char: %ul byte(s)",sizeof(char));
-	printf("Size of int: %ul byte(s)",sizeof(int));
-	printf("Size of long int: %ul byte(s)",sizeof(long int));
-	printf("Size of float: %ulbytes(s)",sizeof(float));
+	printf("Size of a char: %ul byte(s)", sizeof(char));
+	printf("Size of int: %ul byte(s)", sizeof(int));
+	printf("Size of long int: %ul byte(s)", sizeof(long int));
+	printf("Size of float: %ulbytes(s)", sizeof(float));
 	return (0);
 }
