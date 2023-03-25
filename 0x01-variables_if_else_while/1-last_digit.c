fc0c13bd8222994bbc5b1daae73e4681396ea705
IginlaIsrael <iginlaisraelo@gmail.com>
2023-03-25 15:57:47 -0700

    strings

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	else if (v == 0)
 		printf("Last digit of %d is %d and is 0", n, v);
 	else if (v < 6 && v != 0)
-		printf("Last digit of %d is %d and is less than 6 and not zero", n, v);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, v);
 	printf("\n");
 
 	return (0);
