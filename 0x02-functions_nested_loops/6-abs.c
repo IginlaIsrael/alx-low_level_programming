94b03778c7af071345a3e604234dfb469ae70878
IginlaIsrael <iginlaisraelo@gmail.com>
2023-03-28 13:02:24 -0700

    wow

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
+ *_abs - function for absolute value
  *
- *
- *
- *
+ *@s: the absolute value parameter to be printed
+ *Return: returns a always
  */
 
-int _abs(int)
+int _abs(int s)
 {
-	
+	if (s < 0)
+		s = -(s);
+	else if (s >= 0)
+		s = s;
+	return (s);
 }
