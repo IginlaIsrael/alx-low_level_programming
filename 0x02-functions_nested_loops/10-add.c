29189f52b760e7904228f6fc32a741489a6c18f4
IginlaIsrael <iginlaisraelo@gmail.com>
2023-03-28 16:29:54 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ *@i: parameter to be added
+ *@j: parameter to be added
+ * Return: returns sum
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication table from zero to nine.
+ *
+ * Return: is 0
+ */
+
+void times_table(void);
+{
+}
