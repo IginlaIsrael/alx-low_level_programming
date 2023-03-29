cee5e6f52b74c25c08b520b51545322561d4a297
IginlaIsrael <iginlaisraelo@gmail.com>
2023-03-28 17:48:46 -0700

    www

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,16 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+
+	if (n < 0)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
@@ -29,5 +38,4 @@ void print_to_98(int n)
 	}
 	else if (n == 98)
 		printf("%d, ", n);
-	printf("\n");
 }
