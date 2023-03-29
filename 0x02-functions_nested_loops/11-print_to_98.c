780a1609d525950675113deaac5aceef49b82021
IginlaIsrael <iginlaisraelo@gmail.com>
2023-03-28 18:18:42 -0700

    ee

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,6 @@ void print_to_98(int n)
 			if (i != 98)
 				printf(", ");
 		}
-		printf("\n");
 	}
 	else if (n < 0)
 	{
@@ -29,7 +28,6 @@ void print_to_98(int n)
 			if (i != 98)
 				printf(", ");
 		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
@@ -39,9 +37,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf(", ");
 		}
-		printf("\n");
 	}
 	else if (n == 98)
 		printf("%d", n);
-	printf("\n");
 }
