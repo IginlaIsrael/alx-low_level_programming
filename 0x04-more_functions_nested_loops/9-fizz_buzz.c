bffcff1a17d537269f6aa833204afaa40d7ff7c2
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-01 06:39:37 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,11 +19,9 @@ int main(void)
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
 		else if (i == 1)
-			printf("%d ",i);
-		else if (i == 100)
-			printf("%d", i);
+			printf("%d",i);
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
