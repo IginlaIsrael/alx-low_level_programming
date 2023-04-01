4de71ca2f4dfc8c2f13d45534159724afee617b0
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-01 02:08:00 -0700

    I am doing this fr bro it`s so amazing

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - The function that multiplies two integers
+ *
+ * @a: The parameter to be multiplied
+ *
+ * @b: The character/integer to be multiplied
+ *
+ * Return: Returns the multiplied number
+ */
+
+int mul(int a, int b)
+{
+	int multi;
+
+
+	multi = (a * b);
+	return (multi);
+}
