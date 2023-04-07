eb9bfba1613d59d62e19e2d0e067e3655783ecbf
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-06 17:59:11 -0700

    Length_Of_a_string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion
+ * @s: The string length to be found
+ * Return: IS 0 when successful
+ */
+
+int _strlen_recursion(char *s)
+{
+	int israel = 0;
+
+	if (*s)
+	{
+		israel++;
+		israel += _strlen_recursion(s + 1)
+	}
+	return (israel);
+}
