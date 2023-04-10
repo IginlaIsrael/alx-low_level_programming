f7500698f496a24f9aafe54c2355f79578353981
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-10 05:47:05 -0700

    eww

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - A function that concatenates two strings
+ * @dest: One of the strings to be concartenated
+ * @src: The second string to be concartenated
+ * Return: Returns a pointer to the function
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory I am answering the questions on 0x06. C - More pointers, arrays and strings which was a project given to me as a student of the alx comitee
