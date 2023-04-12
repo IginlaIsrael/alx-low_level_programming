d5df3e7cdbf493a46d41ef9ea93abfecce6c9116
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-12 06:22:15 -0700

    www

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * str_concat - A function that concatenates two strings
+ * @s1: A string to be concatenated
+ * @s2: A string to be concartenated
+ * Return: s1 with contents of both strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+}
