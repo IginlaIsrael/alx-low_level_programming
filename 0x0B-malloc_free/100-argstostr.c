55e250a8df99fabf9835df65d41d5b5385bb74e5
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-12 14:35:55 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr -  a function that concatenates all arguments in a program
+ * @ac: An argument to be concartenated
+ * @av: An argument to be concartenated
+ * Return: A pointer to a new string on success and NULL if false
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+}
