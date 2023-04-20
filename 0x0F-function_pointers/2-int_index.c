cd4f246e75ec11b9ea2971f5a1bdf0856a36c761
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-20 12:23:40 -0700

    indexing

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,11 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a;
 
-	if (array == NULL || cmp == NULL || size <= 0 )
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	for (a = 0 ; a < size ; a++)
 	{
-		if(cmp(array[a]))
+		if (cmp(array[a]))
 		return (a);
 	}
 	return (-1);
