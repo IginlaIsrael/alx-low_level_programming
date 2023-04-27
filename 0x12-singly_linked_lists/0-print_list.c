6cd83698fc9f0683227fb667be945e734bfeaddf
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-27 07:59:10 -0700

    First_Commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_list -  a function that prints all the elements of a list_t list.
+ * @h: This is a parameter of the function
+ *
+ * Return: Returns the number of nodes if the string isn`t empty
+ * and if it is, it returns null
+ */
+
+size_t print_list(const list_t *h)
+{
+}
