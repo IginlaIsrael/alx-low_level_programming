1bee7038cc4cf1ea215157eb22c868b292bb1ebd
IginlaIsrael <iginlaisraelo@gmail.com>
2023-04-27 21:22:04 -0700

    tortise_and_hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Israel - Prints some strings before the main function is executed.
+ *
+ * Return: Is nothing
+ */
+
+void Israel(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
